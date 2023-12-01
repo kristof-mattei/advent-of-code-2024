e4ed97c47290d2b7773776d5416593a906e35aa6
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2023-12-01 13:43:06 -0700

    feat: day 01

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,129 +1,185 @@
 use crate::shared::{Day, PartSolution};
 
-fn get_max(lines: &[&str]) -> u32 {
-    let mut max = 0;
+fn first_number_with_index(line: &str) -> Option<(usize, u32)> {
+    line.chars()
+        .enumerate()
+        .find_map(|(index, c)| c.to_digit(10).map(|d| (index, d)))
+}
 
-    let mut current = 0;
+fn last_number_with_index(line: &str) -> Option<(usize, u32)> {
+    line.chars()
+        .rev()
+        .enumerate()
+        .find_map(|(index, c)| c.to_digit(10).map(|d| (line.len() - index - 1, d)))
+}
 
-    // chain because the .lines() skips the final empty line
-    for line in lines.iter().chain(&[""]) {
-        if line.is_empty() {
-            if current > max {
-                max = current;
+fn calculate_total_calibration_value_part_1(lines: &[&str]) -> u32 {
+    let mut total = 0;
+    for line in lines {
+        let (_, first_number) = first_number_with_index(line).expect("No number found");
+        let (_, last_number) = last_number_with_index(line).expect("No number found");
+
+        let total_this_line = first_number * 10 + last_number;
+
+        total += total_this_line;
+    }
+
+    total
+}
+
+const NUMBER_WORDS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn calculate_total_calibration_value_part_2(lines: &[&str]) -> u32 {
+    let mut total = 0;
+    for line in lines {
+        let first_number = first_number_with_index(line);
+        let last_number = last_number_with_index(line);
+
+        let first_word = first_word_with_index(line);
+        let last_word = last_word_with_index(line);
+
+        let first = match (first_number, first_word) {
+            (None, None) => panic!("No number found"),
+            (None, Some((_, first))) | (Some((_, first)), None) => first,
+            (Some((first_number_index, first_number)), Some((first_word_index, first_word))) => {
+                if first_number_index < first_word_index {
+                    first_number
+                } else {
+                    first_word
+                }
+            },
+        };
+
+        total += first * 10;
+
+        let last = match (last_number, last_word) {
+            (None, None) => panic!("No number found"),
+            (None, Some((_, last))) | (Some((_, last)), None) => last,
+            (Some((last_number_index, last_number)), Some((last_word_index, last_word))) => {
+                if last_number_index > last_word_index {
+                    last_number
+                } else {
+                    last_word
+                }
+            },
+        };
+
+        total += last;
+    }
+    total
+}
+
+fn first_word_with_index(line: &str) -> Option<(usize, u32)> {
+    for i in 0..line.len() {
+        for (word_index, word) in NUMBER_WORDS.iter().enumerate() {
+            if line[i..].starts_with(word) {
+                return Some((i, word_index as u32 + 1));
             }
-
-            current = 0;
-            continue;
         }
-
-        current += line.parse::<u32>().unwrap();
     }
 
-    max
+    None
 }
 
-fn shift(current: u32, max: &mut [u32; 3]) {
-    let shift_after = max.iter().position(|v| v < &current);
-
-    let Some(index) = shift_after else { return };
-
-    let mut c_i = max.len() - 1;
-
-    while c_i > (index) {
-        max.swap(c_i, c_i - 1);
-
-        c_i -= 1;
-    }
-
-    max[index] = current;
-}
-
-fn get_top_3(lines: &[&str]) -> u32 {
-    let mut max = [0; 3];
-
-    let mut current = 0;
-
-    // chain because the .lines() skips the final empty line
-    for line in lines.iter().chain(&[""]) {
-        if line.is_empty() {
-            shift(current, &mut max);
-
-            current = 0;
-            continue;
+fn last_word_with_index(line: &str) -> Option<(usize, u32)> {
+    for i in (0..line.len()).rev() {
+        for (word_index, word) in NUMBER_WORDS.iter().enumerate() {
+            if line[i..].starts_with(word) {
+                return Some((i, word_index as u32 + 1));
+            }
         }
-
-        current += line.parse::<u32>().unwrap();
     }
 
-    max.iter().sum()
+    None
 }
-
 pub struct Solution {}
 
 impl Day for Solution {
     fn part_1(&self) -> PartSolution {
         let lines: Vec<&str> = include_str!("input.txt").lines().collect();
 
-        let max = get_max(&lines);
-
-        max.into()
+        calculate_total_calibration_value_part_1(&lines).into()
     }
 
     fn part_2(&self) -> PartSolution {
         let lines: Vec<&str> = include_str!("input.txt").lines().collect();
 
-        let max = get_top_3(&lines);
-
-        max.into()
+        calculate_total_calibration_value_part_2(&lines).into()
     }
 }
 
 #[cfg(test)]
 mod test {
-    fn get_example() -> Vec<&'static str> {
-        include_str!("example.txt")
-            .lines()
-            .map(Into::into)
-            .collect()
-    }
 
     mod part_1 {
-        use super::super::test::get_example;
-        use super::super::{get_max, Solution};
+        use crate::day_01::{calculate_total_calibration_value_part_1, Solution};
         use crate::shared::{Day, PartSolution};
+
+        fn get_example() -> Vec<&'static str> {
+            include_str!("example_part_1.txt")
+                .lines()
+                .map(Into::into)
+                .collect()
+        }
 
         #[test]
         fn outcome() {
-            assert_eq!(PartSolution::U32(67658), (Solution {}).part_1());
+            assert_eq!(PartSolution::U32(54159), (Solution {}).part_1());
         }
 
         #[test]
         fn example() {
             let lines = get_example();
 
-            let max = get_max(&lines);
+            let max = calculate_total_calibration_value_part_1(&lines);
 
-            assert_eq!(max, 24000);
+            assert_eq!(max, 142);
         }
     }
 
     mod part_2 {
-        use super::super::test::get_example;
-        use super::super::{get_top_3, Solution};
+        use crate::day_01::{calculate_total_calibration_value_part_2, Solution};
         use crate::shared::{Day, PartSolution};
 
-        #[test]
-        fn outcome() {
-            assert_eq!(PartSolution::U32(200_158), (Solution {}).part_2());
+        fn get_example() -> Vec<&'static str> {
+            include_str!("example_part_2.txt")
+                .lines()
+                .map(Into::into)
+                .collect()
         }
 
         #[test]
         fn example() {
             let lines = get_example();
 
-            let top3 = get_top_3(&lines);
+            let max = calculate_total_calibration_value_part_2(&lines);
 
-            assert_eq!(top3, 45000);
+            assert_eq!(max, 281);
+        }
+
+        #[test]
+        fn outcome() {
+            assert_eq!(PartSolution::U32(53866), (Solution {}).part_2());
+        }
+
+        #[test]
+        fn first_word_with_index() {
+            let line = "one two three four five six seven eight nine";
+            let (index, number) = super::super::first_word_with_index(line).unwrap();
+
+            assert_eq!(index, 0);
+            assert_eq!(number, 1);
+        }
+
+        #[test]
+        fn last_word_with_index() {
+            let line = "one two three four five six seven eight nine";
+            let (index, number) = super::super::last_word_with_index(line).unwrap();
+
+            assert_eq!(index, 40);
+            assert_eq!(number, 9);
         }
     }
 }
