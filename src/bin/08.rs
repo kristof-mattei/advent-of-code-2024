79e7faa842f25e4eda39713950345e9d832f8951
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2023-12-11 11:47:07 -0700

    chore: fmt

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -139,10 +139,8 @@ impl Parts for Solution {
 #[cfg(test)]
 mod test {
     mod part_1 {
-        use advent_of_code_2023::shared::{
-            solution::{read_file, read_file_part},
-            Parts,
-        };
+        use advent_of_code_2023::shared::solution::{read_file, read_file_part};
+        use advent_of_code_2023::shared::Parts;
 
         use crate::{Solution, DAY};
 
@@ -170,10 +168,8 @@ mod test {
 
     mod part_2 {
 
-        use advent_of_code_2023::shared::{
-            solution::{read_file, read_file_part},
-            Parts,
-        };
+        use advent_of_code_2023::shared::solution::{read_file, read_file_part};
+        use advent_of_code_2023::shared::Parts;
 
         use crate::{Solution, DAY};
 
